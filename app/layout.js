3db2520454687760936f793d6db14325125171a5
denisk13005 <kirmanndenis@gmail.com>
2023-11-27 18:44:00 +0100

    test

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" >
+    <html lang="en" className='h-full'>
       <body className={`h-full ${styles.body}`}>
         <nav className={`w-full h-[8vh]  flex items-center ${styles.navBar}`}>
           <MenuNav />
