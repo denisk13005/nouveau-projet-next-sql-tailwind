58791e0549018e06262764088d904dd7563bfe60
denisk13005 <kirmanndenis@gmail.com>
2023-12-10 10:43:39 +0100

    test

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -7,7 +7,7 @@ export default function profile() {
   return (
     <div>
 
-      <h1 className="text-center text-4xl capitalize p-10">profile {user.pseudo}</h1>
+      <h1 className="text-center text-4xl capitalize p-10">profile {user && user.pseudo}</h1>
     </div>
   )
 }
