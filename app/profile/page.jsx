cc55b52a998d9af71de0c9b9ea35ebb499f9d996
denisk13005 <kirmanndenis@gmail.com>
2023-12-27 13:03:53 +0100

    test test test

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 import { UseUserContext } from '@/context/UserContext'
 import { useRouter } from 'next/navigation'
 
-export default function profile() {
-  // const router = useRouter()
+export default function Profile() {
+  const router = useRouter()
   const { user } = UseUserContext()
-  // if (!user) router.push('/auth')
+  if (!user) router.push('/auth')
   return (
     <div>
 
-      <h1 className="text-center text-4xl capitalize p-10">profiles {user && user.pseudo}</h1>
+      <h1 className="text-center text-4xl capitalize p-10">profil {user && user.pseudo}</h1>
     </div>
   )
 }
