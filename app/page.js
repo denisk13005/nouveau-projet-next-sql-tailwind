d809e8c4b7292f0a687e90588cfe14764436c1c3
denisk13005 <kirmanndenis@gmail.com>
2024-07-10 22:02:33 +0200

    media query home

--- app/page.js
+++ app/page.js
@@ -2,7 +2,12 @@
 export default function Home() {
   return (
     <main className='w-full  relative ' >
-      <h1 className="text-center p-10 text-6xl">Welcome on template for dev</h1>
+      <h1 className="text-center p-10 text-2xl md:text-4xl lg:text-8xl  ">Welcome on  template for dev</h1>
+      <section className="mt-20">
+        <p className="text-center md:text-3xl lg:text-6xl">Here you can find differents templates for beggining your developement project quickly</p>
+        <p className="text-center mt-14 md:text-3xl lg:text-6xl lg:mt-32">We propose differents types of technologies for the front-end and different type of database for save your datas</p>
+        <p className="text-center mt-14 md:text-3xl lg:text-6xl lg:mt-32">All templates come with a <strong><i>signin and signup</i></strong>  form for save your user in your choices database</p>
+      </section>
     </main>
   )
 }
--- components/CardComponent/Card.jsx
+++ components/CardComponent/Card.jsx
@@ -7,8 +7,8 @@ export default function Card({ headerText, photo1, photo2, footerText }) {
       <div className=" text-white h-[25%] flex justify-center items-center text-3xl">{headerText}</div>
       <div className="flex w-full h-[50%] items-center">
 
-        <Image src={photo1} height={175} width={250} style={{ width: '50%', height: '80%' }} />
-        <Image src={photo2} height={175} width={250} style={{ width: '50%', height: '80%' }} />
+        <Image src={photo1} height={175} width={250} style={{ width: '50%', height: '80%' }} alt="photo1logo" />
+        <Image src={photo2} height={175} width={250} style={{ width: '50%', height: '80%' }} alt="photo2logo" />
       </div>
       <div className=" h-[25%] text-white p-3">{footerText}</div>
 
